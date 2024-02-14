2e155d8c30d0f8eb375e2c5be09c0f1770584912
Dmitry Arzhevitin <adachel74@gmail.com>
2024-02-14 16:18:31 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiveIntoCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
