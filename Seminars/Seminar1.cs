ed2ccc60373f756359158caf7d8376d74f77aa5f
Dmitry Arzhevitin <adachel74@gmail.com>
2024-02-14 16:29:39 +0500

    add seminar1

---
+++ Seminars/Seminar1.cs
@@ -0,0 +1,92 @@
+public class Seminar1
+{
+    public void OutView(out int number) // обьяснение out
+    {
+        number = 123123123;
+    }
+    public void Summa(string[] args)  // принять два числа и вывести сумму чисел. Проерить на кол-во переменных.
+    {
+        if (args.Length == 2)
+        {
+            //int number1 = int.Parse(args[0]);
+            //int number2 = Convert.ToInt32(args[1]);
+
+            if (int.TryParse(args[0], out int number1) && int.TryParse(args[1], out int number2))
+            {
+                int summma = number1 + number2;
+                Console.WriteLine($"Сумма = {summma}");
+            }
+            else
+            {
+                Console.WriteLine("Аргументы должны быть числами");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Должно быть два аргумента");
+        }
+    }
+    public void TotalAmount(string[] args) // считает сумму всех чисел
+    {
+        if (args.Length > 0)
+        {
+            int summ = 0;
+            foreach (var item in args)
+            {
+                if (int.TryParse(item, out int number))
+                {
+                    summ = summ + number;
+                }
+
+            }
+            Console.WriteLine($"Сумма всех чисел = {summ}");
+        }
+        else
+        {
+            Console.WriteLine("Нужны аргументы");
+        }
+    }
+    public void MinNunber(string[] args)  // находит минимальное число
+    {
+        if (args.Length > 0)
+        {
+            int minNumber = int.MaxValue;
+            foreach (var item in args)
+            {
+                if (int.TryParse(item, out int number))
+                {
+                    if (number < minNumber)
+                    {
+                        minNumber = number;
+                    }
+                }
+            }
+            Console.WriteLine($"Минимальное число = {minNumber}");
+        }
+        else
+        {
+            Console.WriteLine("Нужны аргументы");
+        }
+    }
+    public void Average(string[] args) // находит среднее арифметическое
+    {
+        if (args.Length > 0)
+        {
+            int summ = 0;
+            int count = 0;
+            foreach (var item in args)
+            {
+                if (int.TryParse(item, out int number))
+                {
+                    summ += number;
+                    count++;
+                }
+            }
+            Console.WriteLine($"Среднее арифметическое = {summ / count}");
+        }
+        else
+        {
+            Console.WriteLine("Нужны аргументы");
+        }
+    }
+}
