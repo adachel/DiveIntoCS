0aced35e597c5d17f0ecfcc086a37d3ebe1f10f8
Dmitry Arzhevitin <adachel74@gmail.com>
2024-02-18 17:48:55 +0500

    homewurk2final

--- HomeWorks/HomeWork2.cs
+++ HomeWorks/HomeWork2.cs
@@ -16,7 +16,6 @@
     public void SortIntArray(int[,] arr) 
     {
         int[] temp = new int[arr.GetLength(0) * arr.GetLength(1)];
-        int[,] newArr = new int[arr.GetLength(0), arr.GetLength(0)];
 
         int count = 0;
 
@@ -27,21 +26,24 @@
                 temp[j + count] = arr[i, j];
             }
             count = count + arr.GetLength(1);
-
         }
+        ViewDualIntArray(arr);
+
+        Console.WriteLine();
+
         Array.Sort(temp);
 
         count = 0;
-        for (int i = 0; i < newArr.GetLength(0); i++)
+        for (int i = 0; i < arr.GetLength(0); i++)
         {
-            for (int j = 0; j < newArr.GetLength(1); j++)
+            for (int j = 0; j < arr.GetLength(1); j++)
             {
-                newArr[i, j] = temp[j + count];
+                arr[i, j] = temp[j + count];
             }
-            count = count + newArr.GetLength(1);
+            count = count + arr.GetLength(1);
         }
 
-        ViewDualIntArray(newArr);
+        ViewDualIntArray(arr);
     }
 
 
